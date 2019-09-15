ecd14a4c6e6da1a7e4dc09960c38e0454c3303de
M. Sohail <sohail8080@hotmail.com>
2019-09-15 16:16:12 +0500

    One DB One DBContext

---
+++ CleanArchTemplate/BC/Home/Domain/Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CleanArchTemplate.BC.Home.Domain.Models
+{
+    public class Class1
+    {
+    }
+}
